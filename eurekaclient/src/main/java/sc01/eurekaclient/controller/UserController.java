915803cdde668c361b2f537e64525f703bd3b2b6
zengly <291893221@qq.com>
2019-05-10 10:38:26 +0800

    swagger

--- eurekaclient/src/main/java/sc01/eurekaclient/controller/UserController.java
+++ eurekaclient/src/main/java/sc01/eurekaclient/controller/UserController.java
@@ -1,16 +1,23 @@
 package sc01.eurekaclient.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *  http://localhost:8083/swagger-ui.html
+ */
 @RestController
+@Api("用户管理")
 public class UserController {
 	/**
 	 * http://localhost:8083/sayHello?username=张三
 	 * @param username
 	 * @return
 	 */
-	@RequestMapping("/sayHello")
+	@GetMapping("/sayHello")
+	@ApiOperation("sayHello|hello,用户！")
 	public Object sayHello(String username){
 		return "hello "+username;
 	}
