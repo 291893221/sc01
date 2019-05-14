ee1d810664072c58bf0297d1376ca3940b6aeb71
zengly <291893221@qq.com>
2019-05-14 14:46:05 +0800

    hystrix

--- consumer/src/main/java/sc01/consumer/service/UserService.java
+++ consumer/src/main/java/sc01/consumer/service/UserService.java
@@ -1,5 +1,6 @@
 package sc01.consumer.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,9 +9,15 @@ import org.springframework.web.client.RestTemplate;
 public class UserService {
 	@Autowired
 	private RestTemplate restTemplate;
+
+	@HystrixCommand(fallbackMethod = "sayHelloFallback")
 	public Object sayHello(String username) {
 		//String forObject = restTemplate.getForObject("http://localhost:8083/sayHello?username="+username, String.class);
 		String forObject = restTemplate.getForObject("http://eurekaclient/sayHello?username="+username, String.class);
 		return forObject;
+	}
+
+	public Object sayHelloFallback(String username) {
+		return "fallbackMethod sayHelloFallback error : "+username;
 	}
 }
